188a75da6159d3835547cedc08e9b84d92879938
bozdemirfatih <fthbzdmr@gmail.com>
2023-03-28 23:50:44 +0200

    day02

---
+++ practice/Fb01.py
@@ -0,0 +1,14 @@
+n = 10
+while n>0:
+    print(n)
+    n = n-1
+print("bittiii")
+print("123"+ "abc")
+sayn = 3*5
+print(sayn)
+
+hrs = input("Enter Hours:")
+hours = float(hrs)
+per = input("Enter Per Hour: ")
+pers = float(per)
+print("Pay: ", float(hours)*pers)
